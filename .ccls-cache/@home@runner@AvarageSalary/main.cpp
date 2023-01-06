94b97f3bc8e5bdbe3fffdd42c8a4403ca383064e
Dovydas Štrupaitis <45108602+ValIIDev@users.noreply.github.com>
2023-01-06 13:04:31 +0000

    Initial commit, working avarage salary feature

--- .ccls-cache/@home@runner@AvarageSalary/main.cpp
+++ .ccls-cache/@home@runner@AvarageSalary/main.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-struct Student
+struct Worker
 {
     string name;
-    int mark;
+    int salary;
 };
 
 int main()
@@ -31,3 +31,72 @@ int main()
   cout << "=======================================" << endl;
     // Open the file "data.txt" for reading
     ifstream input("data.txt");
+
+// Read the data from the file into a vector of Student objects
+vector<Worker> workers;
+string line;
+while (getline(input, line))
+{
+    // Use a stringstream to extract the name and mark from the line
+    stringstream ss(line);
+    string name;
+    int salary;
+    if (ss >> name >> salary)
+    {
+
+        {
+            workers.push_back({ name, salary });
+        }
+    }
+}
+
+    // Check if the vector is empty
+    if (workers.empty()) {
+    cout << "~~~~No data found in the file.~~~" << endl;
+    return 0;
+}
+else {
+    cout << "Data file contains info" << endl;
+}
+
+// Calculate the average mark
+    double total = 0;
+    for (const auto& worker : workers)
+    {
+        total += worker.salary;
+    }
+    double average = total / workers.size();
+    cout << "= Average salary: " << average << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+  }    
+else
+    {
+        cout << "Invalid username or password." << endl;
+    }
+
+    return 0;
+}
